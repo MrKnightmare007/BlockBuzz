5e7782db6c914b58d9a4ed004fb0ed51a1afb0cd
Soumyadipta Das <dassoumyadipta007@gmail.com>
2025-03-03 01:51:27 +0530

    Created a Profile Page

---
+++ client/app/profile/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const style = {
+    wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+    content: `max-w-[1400px] w-2/3 flex justify-between`,
+    mainContent: `flex-[2] border-r border-l border-[#38444d] overflow-y-scroll`,
+  }
+  
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
