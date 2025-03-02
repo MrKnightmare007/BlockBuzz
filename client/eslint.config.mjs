68df3a9d4f7bbf60f9e137193f4b2b93710b1829
Soumyadipta Das <dassoumyadipta007@gmail.com>
2025-03-02 23:40:28 +0530

    Updated ESlint rules

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -13,7 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": "off", // Disable the no-unused-vars rule
+      "@typescript-eslint/no-unused-vars": "off", // Disable unused vars rule
+      "react/no-unescaped-entities": "off",       // Disable unescaped entities rule
     },
   },
 ];
