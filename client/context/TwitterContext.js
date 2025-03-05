7e6d18f44c8ea411dc32a30b3e1913d858cad201
Soumyadipta Das <dassoumyadipta007@gmail.com>
2025-03-05 22:50:39 +0530

    Minor Tweak

--- client/context/TwitterContext.js
+++ client/context/TwitterContext.js
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { client } from '../lib/client'
 
 export const TwitterContext = createContext()
